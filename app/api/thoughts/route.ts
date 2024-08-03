a0d8874b5145802c3940db69f0e094176c6539da
Neel Champaneria <neel@promactinfo.com>
2024-08-03 16:07:02 +0530

    Nodejs runtime

--- app/api/thoughts/route.ts
+++ app/api/thoughts/route.ts
@@ -2,20 +2,18 @@ import connectMongoDB from "@/libs/mongodb";
 import Thought from "@/models/thoughts";
 import { NextRequest, NextResponse } from "next/server";
 
-export const runtime = "edge";
-
 export const preferredRegion = ["bom1"];
 
 export async function POST(request: NextRequest) {
   const { text } = await request.json();
   await connectMongoDB();
   await Thought.create({ text });
-  return NextResponse.json({ message: "Thought Created" }, { status: 201 });
+  return Response.json({ message: "Thought Created" }, { status: 201 });
 }
 
 export async function GET(request: NextRequest) {
   await connectMongoDB();
   const thoughts = await Thought.find();
   console.log(`from ${process.env.VERCEL_REGION}`);
-  return NextResponse.json(thoughts);
+  return Response.json(thoughts);
 }
